26e7cca81547ea514e3efcbd5a97129f18f7230b
JulimPark <100160055+JulimPark@users.noreply.github.com>
2025-07-25 14:53:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -47,11 +47,11 @@ if st.session_state.logged_in == False:
 
     with st.form('로그인'):
         st.subheader(':blue[KIOT 빅데이터팀] 업무일지')
-        cols = st.columns(3)
+        cols = st.columns([1,4,1])
         with cols[1]:
             name = st.text_input('ID')
             password = st.text_input('Password',type='password')
-            loginbtn = st.form_submit_button(label='Login')
+            loginbtn = st.form_submit_button(label='Login',use_container_width=True)
 
     if loginbtn:
         if name in members:
